b11b3de26e7c0e2d1a0fb8921f6d22148be8eb91
programmer-ethan <scarlet742@gmail.com>
2021-05-22 09:42:12 +0900

    make runnable with xml of home

--- app/src/main/java/com/professionalandroid/apps/coin_in_us/ui/home/HomeFragment.java
+++ app/src/main/java/com/professionalandroid/apps/coin_in_us/ui/home/HomeFragment.java
@@ -22,7 +22,7 @@ public class HomeFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_coin_search, container, false);
+        View root = inflater.inflate(R.layout.fragment_interested, container, false);//false
         final TextView textView = root.findViewById(R.id.text_title);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
