cd0396a44220ef21ccdaf36dabfd54b8c1995ebb
seoyoung1006 <qpxl9998@naver.com>
2021-05-24 01:35:11 +0900

    bitcoin_search

--- app/src/main/java/com/professionalandroid/apps/coin_in_us/MainActivity.java
+++ app/src/main/java/com/professionalandroid/apps/coin_in_us/MainActivity.java
@@ -1,6 +1,7 @@
 package com.professionalandroid.apps.coin_in_us;
 
 import android.os.Bundle;
+import android.widget.SearchView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -10,19 +11,26 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.util.ServiceConfigurationError;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SearchView searchView;
+        searchView = findViewById(R.id.search_view);
+
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavController navController = Navigation.findNavController(this, R.id.nav_view);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
