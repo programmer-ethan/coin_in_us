e626fe5f97158202f16868ba29b2d2a68a30ca90
programmer-ethan <scarlet742@gmail.com>
2021-05-25 16:40:45 +0900

    conflict solve

--- app/src/main/java/com/professionalandroid/apps/coin_in_us/MainActivity.java
+++ app/src/main/java/com/professionalandroid/apps/coin_in_us/MainActivity.java
@@ -19,20 +19,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        SearchView searchView;
-        searchView = findViewById(R.id.search_view);
-
-
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                 R.id.navigation_coin, R.id.navigation_home, R.id.navigation_stock)
+                R.id.navigation_coin, R.id.navigation_home, R.id.navigation_stock)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_view);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
 
 }
+//coin _ search branch....
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        SearchView searchView;
+//        searchView = findViewById(R.id.search_view);
+//        BottomNavigationView navView = findViewById(R.id.nav_view);
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                 R.id.navigation_coin, R.id.navigation_home, R.id.navigation_stock)
+//                .build();
+//        NavController navController = Navigation.findNavController(this, R.id.nav_view);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupWithNavController(navView, navController);
+//    }
+//}
