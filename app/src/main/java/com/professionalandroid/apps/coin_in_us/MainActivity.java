ad0dfcf180c003b020f2b4a7954235aae4e7d21b
request10hour <request10hour@gmail.com>
2021-06-01 17:38:19 +0900

    531

--- app/src/main/java/com/professionalandroid/apps/coin_in_us/MainActivity.java
+++ app/src/main/java/com/professionalandroid/apps/coin_in_us/MainActivity.java
@@ -1,24 +1,17 @@
 package com.professionalandroid.apps.coin_in_us;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.SearchView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import java.util.ServiceConfigurationError;
-
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,19 +25,5 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-    }
-    public void writeSharedPreference(View view){
-//        EditText txtValue = (EditText) findViewById(R.id.saved_data);
-//        String value = txtValue.getText().toString();
-        // 1. get Shared Preference
-        SharedPreferences sharedPreference
-                = this.getSharedPreferences("MYPREFRENCE", Context.MODE_MULTI_PROCESS | Context.MODE_WORLD_READABLE);
-        // 2. get Editor
-        SharedPreferences.Editor editor = sharedPreference.edit();
-        // 3. set Key values
-        editor.putString("MYKEY","VALUE1");
-        editor.putString("KEY2", "VALUE2");
-        // 4. commit the values
-        editor.commit();
     }
 }
--- app/src/main/java/com/professionalandroid/apps/coin_in_us/ui/home/HomeFragment.java
+++ app/src/main/java/com/professionalandroid/apps/coin_in_us/ui/home/HomeFragment.java
@@ -2,40 +2,41 @@ package com.professionalandroid.apps.coin_in_us.ui.home;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
+
 import android.os.Bundle;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.TextView;
+
 
 import androidx.annotation.NonNull;
+
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.professionalandroid.apps.coin_in_us.R;
-import com.professionalandroid.apps.coin_in_us.MainActivity;
-
 public class HomeFragment extends Fragment{
 
     private HomeViewModel homeViewModel;
 
-    public void writeSharedPreference(View view){
-//        EditText txtValue = (EditText) findViewById(R.id.saved_data);
-//        String value = txtValue.getText().toString();
-    // 1. get Shared Preference
-    Context context = getActivity();
-    SharedPreferences sharedPreference
-            = context.getSharedPreferences("MYPREFRENCE", Context.MODE_MULTI_PROCESS | Context.MODE_PRIVATE);
-    // 2. get Editor
-    SharedPreferences.Editor editor = sharedPreference.edit();
-    // 3. set Key values
-    editor.putString("MYKEY","VALUE1");
-    editor.putString("KEY2", "VALUE2");
-    // 4. commit the values
-    editor.commit();
-}
+//    public void writeSharedPreference(View view){
+////        EditText txtValue = (EditText) findViewById(R.id.saved_data);
+////        String value = txtValue.getText().toString();
+//    // 1. get Shared Preference
+//    Context context = getActivity();
+//    SharedPreferences sharedPreference
+//            = context.getSharedPreferences("MYPREFRENCE", Context.MODE_MULTI_PROCESS | Context.MODE_PRIVATE);
+//    // 2. get Editor
+//    SharedPreferences.Editor editor = sharedPreference.edit();
+//    // 3. set Key values
+//    editor.putString("MYKEY","VALUE1");
+//    editor.putString("KEY2", "VALUE2");
+//    // 4. commit the values
+//    editor.commit();
+//}
+
+
 //    public void readPreference(View view) throws Exception {
 //        // get Context of other application
 //        Context context = getActivity();
@@ -49,14 +50,13 @@ public class HomeFragment extends Fragment{
 ////        txtValue.setText(value);
 //    }
 
-
-
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_interested, container, false);//false
+
+//SharedPreferences Write
         Context context = getActivity();
         SharedPreferences sharedPreference
                 = context.getSharedPreferences("MYPREFRENCE", Context.MODE_MULTI_PROCESS | Context.MODE_PRIVATE);
@@ -69,6 +69,7 @@ public class HomeFragment extends Fragment{
         editor.putString("STOCK_VALUE_1", "59,440,000");
         // 4. commit the values
         editor.commit();
+//SharedPreferences Read
         // getting Shared preference from other application
         SharedPreferences pref
                 = context.getSharedPreferences("MYPREFRENCE", Context.MODE_PRIVATE | Context.MODE_MULTI_PROCESS);
@@ -76,14 +77,34 @@ public class HomeFragment extends Fragment{
         String value1 = pref.getString("COIN_VALUE_1", "NOTFOUND");
         String name2 = pref.getString("STOCK_NAME_1", "NOTFOUND");
         String value2 = pref.getString("STOCK_VALUE_1", "NOTFOUND");
-        TextView txtValue = root.findViewById(R.id.coin_name_1);
-        txtValue.setText(name1);
-        TextView txtValue2 = root.findViewById(R.id.coin_value_1);
-        txtValue2.setText(value1);
-        TextView txtValue3 = root.findViewById(R.id.stock_name_1);
-        txtValue3.setText(name2);
-        TextView txtValue4 = root.findViewById(R.id.stock_value_1);
-        txtValue4.setText(value2);
+
+//테이블에 텍스트 삽입
+//        TextView txtValue = root.findViewById(R.id.coin_name_1);
+//        txtValue.setText(name1);
+//        TextView txtValue2 = root.findViewById(R.id.coin_value_1);
+//        txtValue2.setText(value1);
+//        TextView txtValue3 = root.findViewById(R.id.stock_name_1);
+//        txtValue3.setText(name2);
+//        TextView txtValue4 = root.findViewById(R.id.stock_value_1);
+//        txtValue4.setText(value2);
+
+//동적 테이블
+//        TableLayout tableLayout;
+//
+//        tableLayout = root.findViewById(R.id.cointable);
+//        TableRow tableRow = new TableRow(context);
+//        tableRow.setLayoutParams(new TableRow.LayoutParams(
+//                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+//        for(int i=0;i<3;i++){
+//            TextView textView = new TextView(context);
+//            textView.setText(name2);
+//            textView.setGravity(Gravity.CENTER);
+//            textView.setTextSize(18);
+////            textView.setBackground(ContextCompat.getDrawble(context, table_inside));
+//            textView.setBackgroundResource(table_inside);
+//            tableRow.addView(textView);
+//        }
+//        tableLayout.addView(tableRow);
 
 
 
